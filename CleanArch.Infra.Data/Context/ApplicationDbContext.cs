f906f092cd8e53a58a0531c4e25a04156f9d5e82
Alaor <Alaor@ALOR>
2025-04-05 17:48:17 -0400

    Primeiras implemetacoes no infra.data

---
+++ CleanArch.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using CleanArch.Domain.Entities;
+using CleanArch.Infra.Data.EntitiesConfiguration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Corre a aplicação para encontrar os arquivos que possuem configurações pré estabelecidas, nesse caso, os arquivos na pasta EntitiesConfiguratiom
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            // Forma de fazer individualmente
+            //modelBuilder.ApplyConfiguration(new CategoryConfiguration);
+        }
+    }
+}
---
+++ CleanArch.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.EntitiesConfiguration
+{
+    // feito para quando rodar a migration sair com precisao e tamanho pré definidos
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
+
+            // Acrescenta registros no banco na tabela category
+            builder.HasData(
+                new Category(1, "Material Escolar"),
+                new Category(2, "Eletrônicos"),
+                new Category(3, "Acessórios")
+                );
+        }
+    }
+}
