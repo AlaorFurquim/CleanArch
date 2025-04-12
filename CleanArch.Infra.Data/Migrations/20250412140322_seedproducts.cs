22c65265668b469f81de8061e036b5cc5866e50d
Alaor <Alaor@ALOR>
2025-04-12 10:12:13 -0400

    feito a implementacao do DependcyInjection e adicionado as primeiras Migrations

---
+++ CleanArch.Infra.Data/Migrations/20250412140322_seedproducts.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArch.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedproducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)" + 
+                "VALUES('Caderno Hulk','Caderno Hulk 250 Folhas',25.25,100,'caderno.jpg',1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Products");
+        }
+    }
+}
---
+++ CleanArch.Infra.Ioc/DependecyInjection.cs
@@ -0,0 +1,29 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Ioc
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
+                ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<IcategoryRepository, CategoryRepository>();
+            services.AddScoped<IproductRepository, ProductRepository>();
+
+            return services;
+        }
+    }
+}
