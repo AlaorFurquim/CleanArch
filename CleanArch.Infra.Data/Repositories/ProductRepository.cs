d24bd37031b1574dde169f7c3ca4f77658467d84
Alaor <Alaor@ALOR>
2025-04-12 08:17:40 -0400

    Criado a classe concreta ProductRepository

---
+++ CleanArch.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,60 @@
+﻿using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class ProductRepository : IproductRepository
+    {
+        ApplicationDbContext _productContext;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _productContext = context;
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            _productContext.Products.Add(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> GetByIdAsync(int? id)
+        {
+            return await _productContext.Products.FindAsync(id);
+        }
+
+        public async Task<Product> GetProductCategoryId(int? id)
+        {
+            //eager loanding
+            return await _productContext.Products.Include(x => x.CategoryId)
+                .SingleOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _productContext.Products.ToListAsync();
+        }
+
+        public async Task<Product> RemoveAsync(Product product)
+        {
+            _productContext.Products.Remove(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            _productContext.Products.Update(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+    }
+}
